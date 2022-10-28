eb669e1f4fd29a53c4a400210548b1b605186684
danny.daniil <danny.daniil@uniswap.org>
2022-10-28 17:33:44 -0400

    update git ignore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-console.log('Hello world')
-
-const foo = 5
-export default foo
+export function helloWorld() {
+    const message = 'Hello World from my example modern npm package!';
+    return message;
+  }
+  
+  export function goodBye() {
+    const message = 'Goodbye from my example modern npm package!';
+    return message;
+  }
+  
+  export default {
+    helloWorld,
+    goodBye,
+  };
