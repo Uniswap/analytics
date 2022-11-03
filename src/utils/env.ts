bb5591eee2486933552fc4d49a2b84ff6df81b46
danny.daniil <danny.daniil@uniswap.org>
2022-11-03 11:11:16 -0400

    lint workflow

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,8 +1,7 @@
 export function isStagingEnv(): boolean {
-    return Boolean(process.env.REACT_APP_STAGING)
-  }
-  
-  export function isProductionEnv(): boolean {
-    return process.env.NODE_ENV === 'production' && !isStagingEnv()
-  }
-  
+  return Boolean(process.env.REACT_APP_STAGING)
+}
+
+export function isProductionEnv(): boolean {
+  return process.env.NODE_ENV === 'production' && !isStagingEnv()
+}
