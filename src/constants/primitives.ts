37986433a472624309ca8a0ea9364d99e863b092
danny.daniil <danny.daniil@uniswap.org>
2022-11-03 17:34:17 -0400

    alphabetize primitives

--- src/constants/primitives.ts
+++ src/constants/primitives.ts
@@ -5,16 +5,15 @@
  * and logged.
  */
 export enum EventName {
-  APP_LOADED = 'Application Loaded',
   APPROVE_TOKEN_TXN_SUBMITTED = 'Approve Token Transaction Submitted',
+  APP_LOADED = 'Application Loaded',
   CONNECT_WALLET_BUTTON_CLICKED = 'Connect Wallet Button Clicked',
   EXPLORE_BANNER_CLICKED = 'Explore Banner Clicked',
   EXPLORE_SEARCH_SELECTED = 'Explore Search Selected',
   EXPLORE_TOKEN_ROW_CLICKED = 'Explore Token Row Clicked',
-  PAGE_VIEWED = 'Page Viewed',
   NAVBAR_RESULT_SELECTED = 'Navbar Result Selected',
-  NAVBAR_SEARCH_SELECTED = 'Navbar Search Selected',
   NAVBAR_SEARCH_EXITED = 'Navbar Search Exited',
+  NAVBAR_SEARCH_SELECTED = 'Navbar Search Selected',
   NFT_ACTIVITY_SELECTED = 'NFT Activity Selected',
   NFT_BUY_ADDED = 'NFT Buy Bag Added',
   NFT_BUY_BAG_CHANGED = 'NFT Buy Bag Changed',
@@ -25,6 +24,7 @@ export enum EventName {
   NFT_FILTER_OPENED = 'NFT Collection Filter Opened',
   NFT_FILTER_SELECTED = 'NFT Filter Selected',
   NFT_TRENDING_ROW_SELECTED = 'Trending Row Selected',
+  PAGE_VIEWED = 'Page Viewed',
   SWAP_AUTOROUTER_VISUALIZATION_EXPANDED = 'Swap Autorouter Visualization Expanded',
   SWAP_DETAILS_EXPANDED = 'Swap Details Expanded',
   SWAP_MAX_TOKEN_AMOUNT_SELECTED = 'Swap Max Token Amount Selected',
@@ -59,27 +59,27 @@ export enum BrowserEvent {
 }
 
 export enum Browser {
-  FIREFOX = 'Mozilla Firefox',
-  SAMSUNG = 'Samsung Internet',
-  OPERA = 'Opera',
-  INTERNET_EXPLORER = 'Microsoft Internet Explorer',
+  BRAVE = 'Brave',
+  CHROME = 'Google Chrome or Chromium',
   EDGE = 'Microsoft Edge (Legacy)',
   EDGE_CHROMIUM = 'Microsoft Edge (Chromium)',
-  CHROME = 'Google Chrome or Chromium',
+  FIREFOX = 'Mozilla Firefox',
+  INTERNET_EXPLORER = 'Microsoft Internet Explorer',
+  OPERA = 'Opera',
   SAFARI = 'Apple Safari',
-  BRAVE = 'Brave',
+  SAMSUNG = 'Samsung Internet',
   UNKNOWN = 'unknown',
 }
 
 export enum CustomerUserProperties {
   ALL_WALLET_ADDRESSES_CONNECTED = 'all_wallet_addresses_connected',
   ALL_WALLET_CHAIN_IDS = 'all_wallet_chain_ids',
-  USER_AGENT = 'user_agent',
   BROWSER = 'browser',
   DARK_MODE = 'is_dark_mode',
   EXPERT_MODE = 'is_expert_mode',
   SCREEN_RESOLUTION_HEIGHT = 'screen_resolution_height',
   SCREEN_RESOLUTION_WIDTH = 'screen_resolution_width',
+  USER_AGENT = 'user_agent',
   WALLET_ADDRESS = 'wallet_address',
   WALLET_TYPE = 'wallet_type',
 }
