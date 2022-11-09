31f073021e870a3782d15e77fe8e56921a6a095c
dannydaniil <danny.daniil@uniswap.org>
2022-11-09 10:24:00 -0500

    add source of class

--- src/analytics/CustomTransport.ts
+++ src/analytics/CustomTransport.ts
@@ -6,10 +6,13 @@ export enum OriginApplication {
   INTERFACE = 'interface',
 }
 /**
+ *
  * Allows us to set `x-origin-application` to correctly route
  * the application to the correct Amplitude project
  *
  * @param originApplication Name of the application consuming the package. Used to route events to the correct project.
+ *
+ * See example here: https://github.com/amplitude/Amplitude-TypeScript/blob/main/packages/analytics-client-common/src/transports/fetch.ts
  */
 export class CustomTransport extends BaseTransport implements Transport {
   private originApplication: OriginApplication
