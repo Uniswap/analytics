3862480b8b7bf6e78be0fa5c982bcfa9596ea738
Adrian Kant <adrian.kant@uniswap.org>
2024-01-08 17:43:48 -0500

    feat: add anonymous user id constant (#30)

---
+++ src/analytics/constants.ts
@@ -0,0 +1,1 @@
+export const ANONYMOUS_DEVICE_ID = 'anonymous_uniswap_user'
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export {
   user,
 } from './analytics/index.js'
 export { OriginApplication } from './analytics/ApplicationTransport.js'
+export { ANONYMOUS_DEVICE_ID } from './analytics/constants.js'
